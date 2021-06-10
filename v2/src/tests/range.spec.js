4120b9e8471f578ac815cf2a90b8444270989f34
Miguel319 <miguelp90.319@gmail.com>
2021-06-09 20:39:14 -0400

    Created version 2

---
+++ v2/src/tests/range.spec.js
@@ -0,0 +1,9 @@
+const Range = require('../range');
+
+describe("Range", () => {
+  describe("Constructor", () => {
+    it("Should return 4", () => {
+      expect(2 + 2).toBe(4);
+    });
+  });
+});
